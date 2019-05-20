b28d90263843fbe384a996890e1e4aff1a67f202
event <event@c5r5s6.wethinkcode.co.za>
2019-05-20 11:58:10 +0200

    strstr finished (nothing tested properly yet)

--- ft_strstr.c
+++ ft_strstr.c
@@ -1,21 +1,24 @@
 
-char* ft_strstr(const char* haystack, const char* needle)
+char	*ft_strstr(const char *haystack, const char *needle)
 {
-	int cntHay;
-	int cntNee;
-	char* found;
+	int		cntHay;
+	int		cntNee;
+	char	*found;
 
 	cntHay = 0;
-	while(haystack[cntHay] != '\0')
+	while (haystack[cntHay] != '\0')
 	{
 		cntNee = 0;
 		found = &haystack[cntHay];
-		while(haystack[cntHay] == needle[cntNee])
+		while (haystack[cntHay] == needle[cntNee])
 		{
 			cntHay++;
 			cntNee++;
 		}
-		if(needle[cntNee] == '\0')
-			return(found);
-
-
+		if (needle[cntNee] == '\0')
+			return (found);
+		if	(haystack[cntHay] != needle[cntNee])
+			cntHay++;
+	}
+	return (NULL);
+}
