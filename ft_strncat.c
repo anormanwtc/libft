8e1f6f6bab813cb6069eb43f16460afe0d43cc18
Alistair Norman <anorman@c5r10s8.wethinkcode.co.za>
2019-05-21 10:59:45 +0200

    strcat int->size_t and strncat done and norme

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,13 +6,13 @@
 /*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 10:42:03 by anorman           #+#    #+#             */
-/*   Updated: 2019/05/21 10:58:57 by anorman          ###   ########.fr       */
+/*   Updated: 2019/05/21 10:58:11 by anorman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#inlcude "libft.h"
+#include "libft.h"
 
-char	*ft_strcat(char *restrict s1, const char *restrict s2)
+char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n)
 {
 	size_t	cnt1;
 	size_t	cnt2;
@@ -21,7 +21,7 @@ char	*ft_strcat(char *restrict s1, const char *restrict s2)
 	cnt2 = 0;
 	while (s1[cnt1])
 		cnt1++;
-	while (s2[cnt2])
+	while (cnt2 < n)
 	{
 		s1[cnt1] = s2[cnt2];
 		cnt1++;
