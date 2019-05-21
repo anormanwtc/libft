c37c0fb94dfcda5444a798526a21348fb84544e2
Alistair Norman <anorman@c5r10s8.wethinkcode.co.za>
2019-05-21 16:32:12 +0200

    libft and the start of a makefile

---
+++ libft.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 14:47:41 by anorman           #+#    #+#             */
+/*   Updated: 2019/05/21 15:15:31 by anorman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#define rstr restrict
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *rstr dst, const void *rstr src, size_t n);
+void	*ft_memccpy(void *rstr dst, const void *rstr src, int c, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1, size_t n);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
+char	*ft_strcat(char *rstr s1, const char *rstr s2);
+char	*ft_strncat(char *rstr s1, const char *rstr s2, size_t len);
+size_t	ft_strlcat(char *rstr dst, const char *rstr srcm size_t dstsize);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *haystack, const char *needle);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atio(const char *str);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+/*
+**int	toupper(int c);
+**int	tolower(int c);
+*/
+#endif
+
+/*
+** stdlib -> malloc/free, unistd -> write, string -> NULL and size_t
+*/
