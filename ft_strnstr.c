ea67ec8d957a379012b9a0ee28af31e4c13f73d8
Alistair Norman <anorman@c5r10s8.wethinkcode.co.za>
2019-05-21 13:41:33 +0200

    strnstr norm untested

---
+++ ft_strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 09:37:30 by anorman           #+#    #+#             */
+/*   Updated: 2019/05/21 13:41:06 by anorman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	hay;
+	size_t	nee;
+	char	*found;
+
+	hay = 0;
+	while (haystack[hay] != '\0' && hay < len)
+	{
+		nee = 0;
+		found = &haystack[hay];
+		while (haystack[hay] == needle[nee])
+		{
+			hay++;
+			nee++;
+		}
+		if (needle[nee] == '\0')
+			return (found);
+		if (haystack[hay] != needle[nee])
+			hay++;
+	}
+	return (NULL);
+}
