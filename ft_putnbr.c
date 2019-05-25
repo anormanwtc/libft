9fca7201baf3ba796ad64ee18cf5feda970d0598
Alistair Norman <anorman@c5r10s8.wethinkcode.co.za>
2019-05-25 16:35:19 +0200

    int min case fixed

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:35:38 by anorman           #+#    #+#             */
-/*   Updated: 2019/05/23 13:37:57 by anorman          ###   ########.fr       */
+/*   Updated: 2019/05/25 16:35:01 by anorman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 void	ft_putnbr(int nbr)
 {
+	int		neg;
+
+	neg = 1;
 	if (nbr < 0)
 	{
 		ft_putchar('-');
-		nbr *= -1;
+		neg = -1;
 	}
-	if ((nbr / 10) > 0)
-		ft_putnbr(nbr / 10);
+	if ((nbr / 10) * neg > 0)
+		ft_putnbr((nbr / 10) * neg);
 	ft_putchar((nbr % 10) + 48);
 }
