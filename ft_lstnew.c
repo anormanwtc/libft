b5967efcc2feeee94d83e53d3e3d765df5c7d312
Alistair Norman <anorman@c5r10s8.wethinkcode.co.za>
2019-05-29 16:46:11 +0200

    full libft and new lstnew lstdelone not normed

---
+++ ft_lstnew.c
@@ -0,0 +1,18 @@
+
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*new;
+
+	if (!(new = (t_list *)malloc(sizeof(t_list))))
+		return (NULL);
+	new->content = (void *)content;
+	if (new->content)
+		new->content_size = content_size;
+	else
+		new->content_size = 0;
+	new->next = NULL;
+	return (new);
+}
