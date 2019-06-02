50454af0b426ef142c9affa4b847fc4e84c16184
Alistair Norman <anorman@c5r10s8.wethinkcode.co.za>
2019-06-02 14:12:35 +0200

    isthing and toup/low main tester

---
+++ libft_manualtest/isthing_main.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   isthing_main.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/02 14:00:59 by anorman           #+#    #+#             */
+/*   Updated: 2019/06/02 14:12:02 by anorman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <ctype.h>
+
+int		main(void)
+{
+	int c;
+
+	c = -100;
+	while (c < 400)
+	{
+		if (isalpha(c) && ft_isalpha(c));
+		else if (!isalpha(c) && !ft_isalpha(c));
+		else
+			printf("%d\n", c);
+		c++;
+	}
+
+	c = -100;
+	while (c < 400)
+	{
+		if (isdigit(c) && ft_isdigit(c));
+		else if (!isdigit(c) && !ft_isdigit(c));
+		else
+			printf("%d\n", c);
+		c++;
+	}
+
+	c = -100;
+	while (c < 400)
+	{
+		if (isalnum(c) && ft_isalnum(c));
+		else if (!isalnum(c) && !ft_isalnum(c));
+		else
+			printf("%d\n", c);
+		c++;
+	}
+
+	c = -100;
+	while (c < 400)
+	{
+		if (isascii(c) && ft_isascii(c));
+		else if (!isascii(c) && !ft_isascii(c));
+		else
+			printf("%d\n", c);
+		c++;
+	}
+
+	c = -100;
+	while (c < 400)
+	{
+		if (isprint(c) && ft_isprint(c));
+		else if (!isprint(c) && !ft_isprint(c));
+		else
+			printf("%d\n", c);
+		c++;
+	}
+	return (0);
+	
+	c = -100;
+	while (c < 400)
+	{
+		if (toupper(c) != ft_toupper(c))
+			printf("%d\n", c);
+		c++;
+	}
+
+	c = -100;
+	while (c < 400)
+	{
+		if (tolower(c) != ft_tolower(c))
+			printf("%d\n", c);
+		c++;
+	}
+	return (0);
+}
