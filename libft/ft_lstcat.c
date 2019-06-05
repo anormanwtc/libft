25ec1bad4b1a96d06d404c4e5c35d0ec535f15f9
Alistair Norman <anorman@c4r18s3.wethinkcode.co.za>
2019-06-05 17:30:35 +0200

    Makefile fix and lstcat norm

--- libft/ft_lstcat.c
+++ libft/ft_lstcat.c
@@ -6,7 +6,7 @@
 /*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 16:34:43 by anorman           #+#    #+#             */
-/*   Updated: 2019/06/04 16:51:08 by anorman          ###   ########.fr       */
+/*   Updated: 2019/06/05 17:12:24 by anorman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ char	*ft_lstcat(t_list *lst)
 	str[sumsize] = '\0';
 	while (lst)
 	{
-		ft_strcat(str,  (char *)lst->content);
+		ft_strcat(str, (char *)lst->content);
 		lst = lst->next;
 	}
 	return (str);
