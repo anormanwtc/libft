d92696b561fa343c27a7ce6eee2253c520cb5789
Alistair Norman <anorman@c4r18s3.wethinkcode.co.za>
2019-06-05 18:13:38 +0200

    isthing_main expanded and Makefile fixed and readme for subjective differnces

--- libft_manualtest/isthing_main.c
+++ libft_manualtest/isthing_main.c
@@ -6,18 +6,19 @@
 /*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/02 14:00:59 by anorman           #+#    #+#             */
-/*   Updated: 2019/06/02 14:12:02 by anorman          ###   ########.fr       */
+/*   Updated: 2019/06/05 17:58:53 by anorman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <ctype.h>
+#include <stdio.h>
 
 int		main(void)
 {
 	int c;
 
-	c = -100;
+	c = -300;
 	while (c < 400)
 	{
 		if (isalpha(c) && ft_isalpha(c));
@@ -27,7 +28,7 @@ int		main(void)
 		c++;
 	}
 
-	c = -100;
+	c = -300;
 	while (c < 400)
 	{
 		if (isdigit(c) && ft_isdigit(c));
@@ -37,7 +38,7 @@ int		main(void)
 		c++;
 	}
 
-	c = -100;
+	c = -300;
 	while (c < 400)
 	{
 		if (isalnum(c) && ft_isalnum(c));
@@ -47,7 +48,7 @@ int		main(void)
 		c++;
 	}
 
-	c = -100;
+	c = -300;
 	while (c < 400)
 	{
 		if (isascii(c) && ft_isascii(c));
@@ -57,7 +58,7 @@ int		main(void)
 		c++;
 	}
 
-	c = -100;
+	c = -300;
 	while (c < 400)
 	{
 		if (isprint(c) && ft_isprint(c));
@@ -68,7 +69,7 @@ int		main(void)
 	}
 	return (0);
 	
-	c = -100;
+	c = -300;
 	while (c < 400)
 	{
 		if (toupper(c) != ft_toupper(c))
@@ -76,7 +77,7 @@ int		main(void)
 		c++;
 	}
 
-	c = -100;
+	c = -300;
 	while (c < 400)
 	{
 		if (tolower(c) != ft_tolower(c))
