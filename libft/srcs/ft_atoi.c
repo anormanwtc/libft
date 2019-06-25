89c702ddbaf6892f9dfad1d4642597c2ad4a9491
Alistair Norman <anorman@c5r10s9.wethinkcode.co.za>
2019-06-25 16:59:12 +0200

    changing atoi back

--- libft/srcs/ft_atoi.c
+++ libft/srcs/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:33:20 by anorman           #+#    #+#             */
-/*   Updated: 2019/06/25 16:50:48 by anorman          ###   ########.fr       */
+/*   Updated: 2019/06/25 16:56:40 by anorman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,34 +21,10 @@ static int	st_isblank(char c)
 	return (0);
 }
 
-static int	st_numcmp(const char *str, char *max)
-{
-	int over;
-
-	over = 0;
-	while (*str > 47 && *str < 58 && *max)
-	{
-		if (*str > *max)
-			over = 1;
-		str++;
-		max++;
-	}
-	if (*str > 47 && *str < 58)
-		return (1);
-	if (!*max)
-		return (over);
-	return (0);
-}
-
-/*
-** Returns 1 if abs(atoi(str)) will be less or equal intmax
-** Returns 0 if abs(atoi(str)) will be greater than intmax
-*/
-
 int			ft_atoi(const char *str)
 {
-	int		res;
-	int		neg;
+	long int	res;
+	int			neg;
 
 	res = 0;
 	neg = 1;
@@ -62,10 +38,6 @@ int			ft_atoi(const char *str)
 			neg = -1;
 		str++;
 	}
-	if (neg == 1 && st_numcmp(str, "2147483647"))
-		return (0);
-	if (neg == -1 && st_numcmp(str, "2147483648"))
-		return (-1);
 	while (*str > 47 && *str < 58)
 	{
 		res = res * 10 + neg * (*str - 48);
