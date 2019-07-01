e0612fa56ff259cd8f1db0ab832d0ff5b2700264
Alistair Norman <anorman@c5r10s6.wethinkcode.co.za>
2019-07-01 15:40:46 +0200

    minor alteration

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 16:35:31 by anorman           #+#    #+#             */
-/*   Updated: 2019/06/26 15:34:13 by anorman          ###   ########.fr       */
+/*   Updated: 2019/07/01 15:39:37 by anorman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -141,7 +141,7 @@ int			get_next_line(const int fd, char **line)
 	if (place->fd != -2)
 		if (!(*line = ft_lstcat(lst)))
 			place->fd = -2;
-	if (place->fd == 0 && (!*line || !(*line)[0]))
+	if (place->fd == -1 && (!*line || !(*line)[0]))
 		ft_strdel(line);
 	return (st_cleanup(&bookmark, place, &lst));
 }
