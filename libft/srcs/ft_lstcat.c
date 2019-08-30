602afdf3e0d3f88224c655255a3214bd6c266e62
Alistair Norman <anorman@c4r24s3.wethinkcode.co.za>
2019-08-30 09:28:32 +0200

    minor efficiency increase

--- libft/srcs/ft_lstcat.c
+++ libft/srcs/ft_lstcat.c
@@ -6,7 +6,7 @@
 /*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 16:34:43 by anorman           #+#    #+#             */
-/*   Updated: 2019/06/18 17:18:09 by anorman          ###   ########.fr       */
+/*   Updated: 2019/08/30 09:28:08 by anorman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ char	*ft_lstcat(t_list *lst)
 	}
 	if (!(str = malloc((sumsize + 1) * sizeof(char))))
 		return (NULL);
-	ft_bzero(str, sumsize + 1);
+	str[0] = '\0';
 	while (lst)
 	{
 		ft_strcat(str, (char *)lst->content);
@@ -38,7 +38,6 @@ char	*ft_lstcat(t_list *lst)
 
 /*
 ** ft_lstcat requires null terminated content.
-** I may consider doing ft_lstmemcat
 **
 ** ft_lstcat returns an empty string if !lst as NULL is for malloc fail
 */
